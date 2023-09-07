56d44784aafd4310906c59cac4c19dd27e6b4c49
Hadi Golkarian <HadiGolkarian@Gmail.com>
2023-09-07 19:33:32 +0400

    bug fix

--- tools/ufginstall/ufginstall.py
+++ tools/ufginstall/ufginstall.py
@@ -211,7 +211,7 @@ class PngDep(Dep):
 
   def install(self):
     """Installs libpng dependency."""
-    url = 'https://storage.googleapis.com/local-dev-david/chert/giflib-5.1.9.tar.gz'
+    url = 'https://download.sourceforge.net/libpng/libpng-1.6.37.tar.gz'
     path = os.path.join(cfg.src_dir, 'png.zip')
     force = self.forced()
     dl_dir = download_archive(url, path, force)
